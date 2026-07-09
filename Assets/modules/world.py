2653ec22973a0359d8a1b292f52393f62fe04bfa
ovicoon <ovicoon@gmail.com>
2026-07-09 22:26:03 +0900

    get_tile_biome 추가로 인한 Room 상속 문제를 오버라이드로 해결

--- Assets/modules/world.py
+++ Assets/modules/world.py
@@ -644,3 +644,7 @@ class Room(World):
 
         # 정적 오브젝트
         self.entities.extend(self.static_objects)
+
+    def get_tile_biome(self, x: float, y: float) -> biome.Biome:
+        """Room은 청크가 없으므로 캐싱을 건너뛰고 곧바로 특별 바이옴을 반환합니다."""
+        return biome.fifth_biome
